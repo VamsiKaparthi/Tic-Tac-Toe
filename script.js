586ccaa3a6b83f5d87542e1c677f8b7ede9d854b
Vamsi <vk784@snu.edu.in>
2023-05-19 13:07:29 +0530

    Add gameboardArr and gameboardfill function

--- script.js
+++ script.js
@@ -4,5 +4,16 @@ for(let i=0; i<9; i++){
     box.style.border="1px solid black"
     box.style.height="130px"
     box.style.width="130px";
+    box.setAttribute('id',`box${i}`)
     gameboard.appendChild(box);
+}
+
+let gameboardArr = [0,0,0,0,0,0,0,0,0]
+
+let gameboardfill = ()=>{
+    for(let i = 0;i<gameboardArr.length;i++){
+        if(gameboardArr[i]=="x"){
+
+        }
+    }
 }
