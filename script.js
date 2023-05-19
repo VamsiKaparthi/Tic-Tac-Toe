bc174f55aaa6ea34551958510affdc04be5bd44c
Vamsi <vk784@snu.edu.in>
2023-05-19 13:14:42 +0530

    Add Gameboard object and put gameboardArr inside it

--- script.js
+++ script.js
@@ -4,16 +4,29 @@ for(let i=0; i<9; i++){
     box.style.border="1px solid black"
     box.style.height="130px"
     box.style.width="130px";
+    box.style.fontSize = "110px"
+    box.style.textAlign="center"
     box.setAttribute('id',`box${i}`)
     gameboard.appendChild(box);
 }
+let Gameboard = {
+    gameboardArr:[0,0,0,0,0,0,0,0,0]
+}
 
-let gameboardArr = [0,0,0,0,0,0,0,0,0]
 
 let gameboardfill = ()=>{
+    let gameboardArr = Gameboard.gameboardArr
     for(let i = 0;i<gameboardArr.length;i++){
+        let box = document.getElementById(`box${i}`)
         if(gameboardArr[i]=="x"){
-
+            box.innerHTML = "X"
+        }
+        else if(gameboardArr[i]=="o"){
+            box.innerHTML = "O"
+        }
+        else{
+            continue;
         }
     }
 }
+gameboardfill();
